ff545f231d3963e0975aadf5acbf80805f94e3d0
Amanpreet Kaur <aman@o7services.com>
2023-06-13 16:01:53 +0530

    Relative layout added

---
+++ app/src/main/java/com/o7solutions/kotlinjunebatch/RelativeActivity.kt
@@ -0,0 +1,36 @@
+package com.o7solutions.kotlinjunebatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class RelativeActivity : AppCompatActivity() {
+
+    lateinit var btnMinus : Button
+    lateinit var btnAdd : Button
+    lateinit var btnMultiply : Button
+    lateinit var btnDivide : Button
+    lateinit var tvNumber : TextView
+    var number = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative)
+
+        btnMinus = findViewById(R.id.btnMinus)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnMultiply = findViewById(R.id.btnMultiply)
+        btnDivide = findViewById(R.id.btnDivide)
+        tvNumber = findViewById(R.id.tvNumber)
+
+        tvNumber.text = number.toString()
+
+        btnAdd.setOnClickListener {
+            number += 2
+            tvNumber.text = number.toString()
+        }
+
+
+    }
+}
